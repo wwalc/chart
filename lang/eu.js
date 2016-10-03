2d48802b9b0a8d060f458e79538e8f39599b89b8
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-10-03 11:25:41 +0200

    Add Basque translation

---
+++ lang/eu.js
@@ -0,0 +1,17 @@
+/*
+ Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+CKEDITOR.plugins.setLang( 'chart', 'eu', {
+	bar: 'Barrak',
+	chart: 'Grafikoa',
+	chartType: 'Grafiko mota:',
+	dialogTitle: 'Editatu grafikoa',
+	doughnut: 'Eraztuna',
+	height: 'Altuera:',
+	label: 'Etiketa:',
+	line: 'Lerroa',
+	pie: 'Gazta',
+	polar: 'Polarra',
+	value: 'Balioa:'
+} );
